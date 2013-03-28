a31941e77d78b24a89e61c33e04fdd904eda1907
Nikolay Kim <nikolay@mochimedia.com>
2013-03-27 21:45:10 -0700

    code moved to main tulip repo

--- httpclient/__init__.py
+++ httpclient/__init__.py
@@ -2,9 +2,7 @@
 
 from .api import *
 from .protocol import *
-from .server import *
 
 
 __all__ = (api.__all__ +
-           protocol.__all__ +
-           server.__all__)
+           protocol.__all__)
